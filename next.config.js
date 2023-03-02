5049866d2b8eb0492eabf556f8f1a688aab027ce
Animesh Dhakal <dhakalanimesh385@gmail.com>
2023-03-02 08:59:19 +0545

    Fix External Images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https:",
+				host: "**",
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
