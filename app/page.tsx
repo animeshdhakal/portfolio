c9383d151d1dc38e2fc9325df7544ef47f444b48
Animesh Dhakal <dhakalanimesh385@gmail.com>
2023-12-29 20:17:05 +0545

    Creating A New Portfolio

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-full w-screen flex justify-center items-center">
+      <h1 className="text-2xl font-bold text-center">Hi I Am Animesh </h1>
+    </div>
+  );
+}
